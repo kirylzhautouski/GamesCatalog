2dce735b2936181e399b9d6ece08c586bc999042
Kirill <kirillzhelt@gmail.com>
2020-01-15 11:22:28 +0300

    Create IndexView and its template to show on games/

--- gamemuster/gamemuster/settings.py
+++ gamemuster/gamemuster/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gamecatalog.apps.GamecatalogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
