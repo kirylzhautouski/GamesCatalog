fb455815825099e7a9cd2d34a9d82e6332398e0c
Kirill <kirillzhelt@gmail.com>
2020-01-16 13:07:12 +0300

    Create DetailsView with its template on games/details/

--- gamemuster/gamecatalog/urls.py
+++ gamemuster/gamecatalog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'gamecatalog'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('details/', views.DetailsView.as_view(), name='details'),
 ]
