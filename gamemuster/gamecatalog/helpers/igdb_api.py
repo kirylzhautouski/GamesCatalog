c372c5da887aa039aa1ef4d09cc84c6dcfee4d2d
Kirill <kirillzhelt@gmail.com>
2020-01-17 15:03:38 +0300

    Implement get_all_games() method to return n most popular games

--- gamemuster/gamecatalog/helpers/igdb_api.py
+++ gamemuster/gamecatalog/helpers/igdb_api.py
@@ -1,10 +1,26 @@
+import json
+
+import requests
+
+
 # noinspection PyPep8Naming
 class IGDB_API:
 
-    @staticmethod
-    def get_all_games(games_count=10):
+    url = 'https://api-v3.igdb.com/games'
+    headers = {'user-key': '47eeca1282bc48976b6949820fb991f1'}
+
+    @classmethod
+    def get_all_games(cls, games_count=10):
+        r = requests.post("https://api-v3.igdb.com/games", headers=cls.headers,
+                          data=f'fields name,popularity; sort popularity desc; limit {games_count};')
+
+        return json.loads(r.text)
+
+    @classmethod
+    def get_game(cls, game_id):
         pass
 
-    @staticmethod
-    def get_game(game_id):
-        pass
+
+if __name__ == "__main__":
+    games_info = IGDB_API.get_all_games()
+    print(games_info)
