1be82c6708ea3c289f70702f8fa73b86a4a8a3c0
Kirill <kirillzhelt@gmail.com>
2020-01-17 15:30:30 +0300

    Implement private helper method __make_request() to avoid code duplication and use it to implement get_game()

--- gamemuster/gamecatalog/helpers/igdb_api.py
+++ gamemuster/gamecatalog/helpers/igdb_api.py
@@ -14,9 +14,9 @@ class IGDB_API:
     headers = {'user-key': '47eeca1282bc48976b6949820fb991f1'}
 
     @classmethod
-    def get_all_games(cls, games_count=10):
+    def __make_request(cls, data):
         r = requests.post(cls.url, headers=cls.headers,
-                          data=f'fields name; sort popularity desc; limit {games_count};')
+                          data=data)
 
         if r.status_code != 200:
             r.raise_for_status()
@@ -24,13 +24,22 @@ class IGDB_API:
         return json.loads(r.text)
 
     @classmethod
+    def get_all_games(cls, games_count=10):
+        return cls.__make_request(f'fields name; sort popularity desc; limit {games_count};')
+
+    @classmethod
     def get_game(cls, game_id):
-        pass
+        game_info = cls.__make_request(f'fields name; where id = {game_id};')
+
+        if len(game_info) == 0:
+            raise InvalidGameIDError()
+
+        return game_info[0]
 
 
 if __name__ == "__main__":
     games_info = IGDB_API.get_all_games()
     print(games_info)
 
-    # most_popular_game = games_info[0]
-    # print(IGDB_API.get_game(115278))
+    most_popular_game = games_info[0]
+    print(IGDB_API.get_game(115278))
