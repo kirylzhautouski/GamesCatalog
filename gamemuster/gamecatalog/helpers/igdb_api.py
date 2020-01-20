39adaf7b3b9251dcef1e455d04f098f103bab583
Kirill <kirillzhelt@gmail.com>
2020-01-20 14:08:46 +0300

    Rename constant class members to the upper case

--- gamemuster/gamecatalog/helpers/igdb_api.py
+++ gamemuster/gamecatalog/helpers/igdb_api.py
@@ -17,30 +17,30 @@ class InvalidCoverIDError(Exception):
 
 # noinspection PyPep8Naming
 class IGDB_API:
-    base_url = 'https://api-v3.igdb.com/'
+    BASE_URL = 'https://api-v3.igdb.com/'
 
-    games_url = f'{base_url}games'
-    covers_url = f'{base_url}covers'
-    keywords_url = f'{base_url}keywords'
-    genres_url = f'{base_url}genres'
-    platforms_url = f'{base_url}platforms'
-    release_dates_url = f'{base_url}release_dates'
-    screenshots_url = f'{base_url}screenshots'
+    GAMES_URL = f'{BASE_URL}games'
+    COVERS_URL = f'{BASE_URL}covers'
+    KEYWORDS_URL = f'{BASE_URL}keywords'
+    GENRES_URL = f'{BASE_URL}genres'
+    PLATFORMS_URL = f'{BASE_URL}platforms'
+    RELEASE_DATES_URL = f'{BASE_URL}release_dates'
+    SCREENSHOTS_URL = f'{BASE_URL}screenshots'
 
-    keywords_column_name = 'slug'
-    genres_column_name = 'slug'
-    platforms_column_name = 'slug'
-    release_dates_column_name = 'human'
-    screenshots_column_name = 'url'
+    KEYWORDS_COLUMN_NAME = 'slug'
+    GENRES_COLUMN_NAME = 'slug'
+    PLATFORMS_COLUMN_NAME = 'slug'
+    RELEASE_DATES_COLUMN_NAME = 'human'
+    SCREENSHOTS_COLUMN_NAME = 'url'
 
-    headers = {'user-key': '47eeca1282bc48976b6949820fb991f1'}
+    HEADERS = {'user-key': '47eeca1282bc48976b6949820fb991f1'}
 
     MAX_GENRES_FOR_GAME = 3
     MAX_KEYWORDS_FOR_GAME = 3
 
     @classmethod
     def __make_request(cls, url, data):
-        r = requests.post(url, headers=cls.headers, data=data)
+        r = requests.post(url, headers=cls.HEADERS, data=data)
 
         if r.status_code != 200:
             r.raise_for_status()
@@ -87,7 +87,7 @@ class IGDB_API:
     def get_all_games(cls, games_count=10, platform_ids=None, genre_ids=None, user_rating_range=None):
         # Use args to filter games
 
-        games_info = cls.__make_request(cls.games_url, f'fields name,cover,genres,keywords; sort popularity desc;\
+        games_info = cls.__make_request(cls.GAMES_URL, f'fields name,cover,genres,keywords; sort popularity desc;\
         limit {games_count}; ' + cls.__build_filters(platform_ids, genre_ids, user_rating_range))
 
         # Load cover, genres, keywords from their ids
@@ -105,7 +105,7 @@ class IGDB_API:
 
                 genre_ids.update(genre_ids_for_game)
 
-        genres = cls.get_resources_at_url(cls.genres_url, cls.genres_column_name, genre_ids)
+        genres = cls.get_resources_at_url(cls.GENRES_URL, cls.GENRES_COLUMN_NAME, genre_ids)
 
         keyword_ids = set()
         for game_info in games_info:
@@ -117,7 +117,7 @@ class IGDB_API:
 
                 keyword_ids.update(keyword_ids_for_game)
 
-        keywords = cls.get_resources_at_url(cls.keywords_url, cls.keywords_column_name, keyword_ids)
+        keywords = cls.get_resources_at_url(cls.KEYWORDS_URL, cls.KEYWORDS_COLUMN_NAME, keyword_ids)
 
         for game_info in games_info:
             game_info['cover'] = covers_for_games[game_info['id']]
@@ -147,7 +147,7 @@ class IGDB_API:
     @classmethod
     def get_game(cls, game_id):
 
-        game_info = cls.__make_request(cls.games_url, f'fields name, keywords, genres, summary,\
+        game_info = cls.__make_request(cls.GAMES_URL, f'fields name, keywords, genres, summary,\
         release_dates, screenshots, rating, rating_count, aggregated_rating, aggregated_rating_count;\
         where id = {game_id};')
 
@@ -157,16 +157,16 @@ class IGDB_API:
         game_info = game_info[0]
 
         genre_ids = game_info.get('genres')
-        cls.__map_ids_to_names(genre_ids, cls.genres_url, cls.genres_column_name)
+        cls.__map_ids_to_names(genre_ids, cls.GENRES_URL, cls.GENRES_COLUMN_NAME)
 
         keyword_ids = game_info.get('keywords')
-        cls.__map_ids_to_names(keyword_ids, cls.keywords_url, cls.keywords_column_name)
+        cls.__map_ids_to_names(keyword_ids, cls.KEYWORDS_URL, cls.KEYWORDS_COLUMN_NAME)
 
         release_date_ids = game_info.get('release_dates')
-        cls.__map_ids_to_names(release_date_ids, cls.release_dates_url, cls.release_dates_column_name)
+        cls.__map_ids_to_names(release_date_ids, cls.RELEASE_DATES_URL, cls.RELEASE_DATES_COLUMN_NAME)
 
         screenshots_ids = game_info.get('screenshots')
-        cls.__map_ids_to_names(screenshots_ids, cls.screenshots_url, cls.screenshots_column_name)
+        cls.__map_ids_to_names(screenshots_ids, cls.SCREENSHOTS_URL, cls.SCREENSHOTS_COLUMN_NAME)
 
         return game_info
 
@@ -179,7 +179,7 @@ class IGDB_API:
         else:
             data = f'fields game,url; where id = {cover_ids};'
 
-        covers_info = cls.__make_request(cls.covers_url, data)
+        covers_info = cls.__make_request(cls.COVERS_URL, data)
 
         if not len(covers_info):
             raise InvalidCoverIDError()
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     most_popular_game = games[0]
 
-    print(IGDB_API.get_all_resources_at_url(IGDB_API.genres_url, IGDB_API.genres_column_name))
-    print(IGDB_API.get_all_resources_at_url(IGDB_API.platforms_url, IGDB_API.platforms_column_name))
+    print(IGDB_API.get_all_resources_at_url(IGDB_API.GENRES_URL, IGDB_API.GENRES_COLUMN_NAME))
+    print(IGDB_API.get_all_resources_at_url(IGDB_API.PLATFORMS_URL, IGDB_API.PLATFORMS_COLUMN_NAME))
 
     print(IGDB_API.get_game(9912))
