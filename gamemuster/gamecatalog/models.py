40badde6afaae7cbdee21370103bf0c0a9c549a6
Kirill <kirillzhelt@gmail.com>
2020-01-24 12:26:15 +0300

    Create User model

--- gamemuster/gamecatalog/models.py
+++ gamemuster/gamecatalog/models.py
@@ -1,2 +1,14 @@
+from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+
+    email = models.EmailField(unique=True)
+
+    first_name = models.CharField(max_length=36)
+    second_name = models.CharField(max_length=36)
+
+    birthday = models.DateField()
+
+    password = models.CharField(max_length=16)
