5173daa21b729eee17acf0510f9583308cbe082f
Kirill <kirillzhelt@gmail.com>
2020-01-27 13:59:28 +0300

    Edit sign up template styles

--- gamemuster/gamecatalog/forms.py
+++ gamemuster/gamecatalog/forms.py
@@ -6,9 +6,41 @@ from .models import User
 
 class SignUpForm(UserCreationForm):
 
-    birthday = forms.DateField(input_formats=['%d-%m-%Y'])
+    birthday = forms.DateField(input_formats=['%d-%m-%Y'],
+                               widget=forms.fields.TextInput(
+                attrs={'placeholder': 'Enter birthday here...'}
+            ))
+
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Enter password here...'}
+        )
+    )
+
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Confirm password...'}
+        )
+    )
 
     class Meta(UserCreationForm):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name',
-                  'birthday')
+                  'birthday', 'password1', 'password2')
+
+        widgets = {
+            'username': forms.fields.TextInput(
+                attrs={'placeholder': 'Enter username here...'}
+            ),
+            'email': forms.fields.EmailInput(
+                attrs={'placeholder': 'Enter email here...'}
+            ),
+            'first_name': forms.fields.TextInput(
+                attrs={'placeholder': 'Enter first name here...'}
+            ),
+            'last_name': forms.fields.TextInput(
+                attrs={'placeholder': 'Enter last name here...'}
+            ),
+        }
