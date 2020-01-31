80d6ccf1509b41d8904ebc3e1e026d10fc819f58
Kirill <kirillzhelt@gmail.com>
2020-01-31 10:52:45 +0300

    Serve static files in production

--- gamemuster/gamemuster/settings.py
+++ gamemuster/gamemuster/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['still-earth-06042.herokuapp.com',
                  '127.0.0.1',
+                 '0.0.0.0',
                  ]
 
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gamemuster.urls'
@@ -130,8 +132,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 IGDB_API_KEY = os.getenv('IGDB_API_KEY')
