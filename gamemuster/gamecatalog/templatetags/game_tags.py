c7d50ea885529e53cf2999904c1c17689ab42335
Kirill <kirillzhelt@gmail.com>
2020-02-05 15:49:16 +0300

    Create template filter for counting how many users added game to favs

--- gamemuster/gamecatalog/templatetags/game_tags.py
+++ gamemuster/gamecatalog/templatetags/game_tags.py
@@ -11,3 +11,8 @@ def is_favourite(game, user):
         return not Favourite.objects.get(game=game, user=user).is_deleted
     except Favourite.DoesNotExist:
         return False
+
+
+@register.filter
+def fav_count(game):
+    return Favourite.objects.filter(game=game).count()
