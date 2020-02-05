468a899b3fd07f084c0eb7c815d79feabadc433e
Kirill <kirillzhelt@gmail.com>
2020-02-05 15:25:32 +0300

    Use more opportunities given by generic Django views

---
+++ gamemuster/gamecatalog/templatetags/game_tags.py
@@ -0,0 +1,13 @@
+from django import template
+
+from gamecatalog.models import Favourite
+
+register = template.Library()
+
+
+@register.filter
+def is_favourite(game, user):
+    try:
+        return not Favourite.objects.get(game=game, user=user).is_deleted
+    except Favourite.DoesNotExist:
+        return False
