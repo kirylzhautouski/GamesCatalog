31da1d4d25789c850aa8e724cea1e0a58ef9b9f4
Kirill <kirillzhelt@gmail.com>
2020-02-06 15:46:38 +0300

    Create basic serializers and viewsets without considering relations

---
+++ gamemuster/gamesapi/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+from gamecatalog.models import User, Platform, Genre, Keyword, Game
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='gamesapi:user-detail')
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'email', 'birthday']
+
+
+class PlatformSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='gamesapi:platform-detail')
+
+    class Meta:
+        model = Platform
+        fields = ['url', 'id', 'name', 'slug']
+
+
+class GenreSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='gamesapi:genre-detail')
+
+    class Meta:
+        model = Genre
+        fields = ['url', 'id', 'slug']
+
+
+class KeywordSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='gamesapi:keyword-detail')
+
+    class Meta:
+        model = Keyword
+        fields = ['url', 'id', 'slug']
+
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='gamesapi:game-detail')
+
+    class Meta:
+        model = Game
+        fields = ['url', 'id', 'name', 'cover_url', 'summary', 'release_date', 'rating', 'rating_count',
+                  'aggregated_rating', 'aggregated_rating_count']
